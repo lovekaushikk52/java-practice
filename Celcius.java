941f8540bee6f944bb222881f2132bfcaeb909e2
lovekaushikk52 <kaushiklove95119@gmail.com>
2026-09-20 07:19:16 +0530

    added

---
+++ Celcius.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Celcius{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the temp in celcius:");
+        float tempc=sc.nextFloat();
+        float tempf=(tempc*9/5)+32;
+        System.out.println(tempf);
+    }
+}
